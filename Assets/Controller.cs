2d21523cfec4b0c334c52783fa494c07fc806944
axelyoung <madebyaxelyoung@gmail.com>
2022-05-13 18:09:47 -0700

    Initial commit

---
+++ Assets/Controller.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+    public string currentShape;
+
+    private GameObject circle;
+    private SpriteRenderer csr;
+
+    private GameObject triangle;
+    private SpriteRenderer tsr;
+
+    private GameObject square;
+    private SpriteRenderer ssr;
+
+    private GameObject diamond;
+    private SpriteRenderer dsr;
+
+    private GameObject curShape;
+    private Vector2 cShapePos;
+    private SpriteRenderer cursr;
+
+    public Color32 disColor;
+    public Color32 actColor;
+
+    public float seconds;
+
+    private bool canSwap = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        circle = transform.GetChild(0).gameObject;
+        triangle = transform.GetChild(1).gameObject;
+        square = transform.GetChild(2).gameObject;
+        diamond = transform.GetChild(3).gameObject;
+        csr = circle.GetComponent<SpriteRenderer>();
+        tsr = triangle.GetComponent<SpriteRenderer>();
+        ssr = square.GetComponent<SpriteRenderer>();
+        dsr = diamond.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            SwapShapeSquare();
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            SwapShapeDiamond();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SwapShapeTriangle();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            SwapShapeCircle();
+        }
+    }
+
+    public void SwapShapeCircle()
+    {
+        StartCoroutine(SwapShape(circle, csr));
+    }
+
+    public void SwapShapeSquare()
+    {
+        StartCoroutine(SwapShape(square, ssr));
+    }
+
+    public void SwapShapeTriangle()
+    {
+        StartCoroutine(SwapShape(triangle, tsr));
+    }
+
+    public void SwapShapeDiamond()
+    {
+        StartCoroutine(SwapShape(diamond, dsr));
+    }
+
+    public IEnumerator SwapShape(GameObject swap, SpriteRenderer swapsr)
+    {
+        if (canSwap)
+        {
+            currentShape = null;
+            canSwap = false;
+            float elapsedTime = 0;
+            Vector3 startingPos = swap.transform.position;
+            while (elapsedTime < seconds)
+            {
+                if(swap != curShape)
+                {
+                    swap.transform.position = Vector3.Lerp(startingPos, Vector3.zero, (elapsedTime / seconds));
+                    swapsr.color = Color32.Lerp(disColor, actColor, (elapsedTime / seconds));
+                }
+                if(curShape != null)
+                {
+                    curShape.transform.position = Vector3.Lerp(Vector3.zero, cShapePos, (elapsedTime / seconds));
+                    cursr.color = Color32.Lerp(actColor, disColor, (elapsedTime / seconds));
+                }
+                elapsedTime += Time.deltaTime;
+                yield return new WaitForEndOfFrame();
+            }
+            if(swap != curShape)
+            {
+                swap.transform.position = Vector3.zero;
+                swapsr.color = actColor;
+            }
+            if (curShape != null)
+            {
+                curShape.transform.position = cShapePos;
+                cursr.color = disColor;
+            }
+            if(swap != curShape)
+            {
+                cShapePos = startingPos;
+                curShape = swap;
+                cursr = swapsr;
+                currentShape = swap.name;
+            }
+            else
+            {
+                cShapePos = Vector2.zero;
+                curShape = null;
+                cursr = null;
+                currentShape = null;
+            }
+            canSwap = true;
+        }
+
+    }
+}
---
+++ Assets/Generator.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+
+    public Controller cl;
+
+    public GameObject[] enemies;
+
+    public float speed;
+    public float frequency;
+
+    public List<GameObject> enemiesInScene = new List<GameObject>();
+
+    private float dirX;
+    private float dirY;
+
+    public TextMeshProUGUI scoreText;
+
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    void Update()
+    {
+        if(enemiesInScene.Count != 0)
+        {
+            Vector2 dir = enemiesInScene[0].GetComponent<Rigidbody2D>().velocity;
+            Vector2 pos = enemiesInScene[0].transform.position;
+            if (Mathf.Abs(Mathf.Abs(Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg) - Mathf.Abs(Mathf.Atan2(-pos.y, -pos.x) * Mathf.Rad2Deg)) > 0.1f)
+            {
+                if (cl.currentShape == enemiesInScene[0].name)
+                {
+                    StartCoroutine(ScaleDown(enemiesInScene[0]));
+                    score += 1000;
+                    scoreText.text = score.ToString();
+                }
+                StartCoroutine(FadeOut(enemiesInScene[0]));
+                enemiesInScene.RemoveAt(0);
+            }
+        }
+    }
+
+    public IEnumerator SpawnEnemy()
+    {
+        int randEnemy = Random.Range(0, enemies.Length);
+        GameObject curEnemy = Instantiate(enemies[randEnemy]);
+        curEnemy.name = enemies[randEnemy].name;
+        dirX = Random.Range(-1.0f,1.0f);
+        if(Random.Range(0,2) == 1)
+        {
+            dirY = 1 - Mathf.Abs(dirX);
+        }
+        else
+        {
+            dirY = -1 + Mathf.Abs(dirX);
+        }
+        Vector2 dir = new Vector2(dirX, dirY);
+        curEnemy.transform.position = dir * 12;
+        curEnemy.GetComponent<Rigidbody2D>().velocity = -dir * speed;
+        enemiesInScene.Add(curEnemy);
+        yield return new WaitForSeconds(frequency);
+        StartCoroutine(SpawnEnemy());
+    }
+
+    public IEnumerator ScaleDown(GameObject enemy)
+    {
+        enemy.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        float elapsedTime = 0;
+        Vector3 startScale = enemy.transform.localScale;
+        while (elapsedTime < 0.4f)
+        {
+            enemy.transform.localScale = Vector3.Lerp(startScale, Vector3.zero, (elapsedTime / 0.4f));
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        Destroy(enemy);
+    }
+
+    public IEnumerator FadeOut(GameObject enemy)
+    {
+        float elapsedTime = 0;
+        Color32 startColor = enemy.GetComponent<SpriteRenderer>().color;
+        while (elapsedTime < 0.4f)
+        {
+            enemy.GetComponent<SpriteRenderer>().color = Color32.Lerp(startColor, new Color32(startColor.r, startColor.g, startColor.b, 0), (elapsedTime / 0.4f));
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        Destroy(enemy);
+    }
+}
